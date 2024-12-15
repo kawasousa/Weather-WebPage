75c5ead43fa331c7c4ca4ed80df8418463ea9cb0
Kawã Sousa <kawasousacontato@gmail.com>
2024-12-15 13:56:13 -0300

    :sparkles: feat: Adiciona mensagem de cidade não encontrada à página conforme resposta da API

--- js/script.js
+++ js/script.js
@@ -5,8 +5,10 @@ const apiCountryUrlFinish = "/flat/64.png";
 const cityInput = document.querySelector("#city-input");
 const searchBtn = document.querySelector("#search");
 
-const weatherData = document.querySelector("#weather-data")
-const cityElement = document.querySelector("#city")
+const weatherData = document.querySelector("#weather-data");
+
+const noCityInfoElement = document.querySelector("#no-city-info");
+const cityElement = document.querySelector("#city");
 const temperatureElement = document.querySelector("#temperature span");
 const descriptionElement = document.querySelector("#description");
 const weatherIconElement = document.querySelector("#weather-icon");
@@ -15,55 +17,60 @@ const humidityElement = document.querySelector("#humidity span");
 const windElement = document.querySelector("#wind span");
 
 
-const getWeatherData = async(city) => {
+const getWeatherData = async (city) => {
     const apiWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}&lang=pt_br`;
-
     const res = await fetch(apiWeatherUrl);
-    const data = await res.json();
+    if (res.ok) {
+        const data = await res.json();
+        return data;
+    }
+};
 
-    return data;
-}
-
-const showWeatherData = async(city) => {
+const showWeatherData = async (city) => {
     const data = await getWeatherData(city);
 
-    cityElement.innerText = data.name;
-    temperatureElement.innerText = data.temp;
-    descriptionElement.innerText = data.weather[0].description;
-    weatherIconElement.setAttribute("src", `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
-    countryElement.setAttribute("src", apiCountryUrlStart + data.sys.country + apiCountryUrlFinish);
-    temperatureElement.innerText = data.main.temp;
-    humidityElement.innerText = data.main.humidity;
-    windElement.innerText = data.wind.speed;
+    if (data) {
+        cityElement.innerText = data.name;
+        temperatureElement.innerText = data.temp;
+        descriptionElement.innerText = data.weather[0].description;
+        weatherIconElement.setAttribute("src", `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+        countryElement.setAttribute("src", apiCountryUrlStart + data.sys.country + apiCountryUrlFinish);
+        temperatureElement.innerText = data.main.temp;
+        humidityElement.innerText = data.main.humidity + "%";
+        windElement.innerText = data.wind.speed;
 
-    weatherData.classList.remove("hide");
-    updatePageTitle(data.name);
-    updatePageIcon(weatherIconElement.getAttribute("src"));
+        noCityInfoElement.classList.add("hide");
+        weatherData.classList.remove("hide");
+        updatePageTitle(data.name);
+        updatePageIcon(weatherIconElement.getAttribute("src"));
+    } else {
+        weatherData.classList.add("hide");
+        noCityInfoElement.classList.remove("hide");
+        document.title = "Clima";
+    }
 };
 
 const updatePageTitle = (city) => {
-    document.title = "Weather - " + city;
+    document.title = "Clima - " + city;
 }
 
 const updatePageIcon = (url) => {
     let link = document.querySelector("link[rel='shortcut icon'");
-    if(link){
-        link.href = url;
-    }
+    link.href = url;
 }
 
-searchBtn.addEventListener("click", (event)=>{
+searchBtn.addEventListener("click", (event) => {
     event.preventDefault();
-    
+
     const city = cityInput.value;
-    
+
     showWeatherData(city);
 })
 
 cityInput.addEventListener("keyup", (event) => {
-    if(event.code === "Enter"){
+    if (event.code === "Enter") {
         const city = event.target.value;
-        
+
         showWeatherData(city);
     }
-})
+});
